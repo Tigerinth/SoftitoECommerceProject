4b4298d4af01b8f418b9b9c3c0cee643c489dd08
Emir <emirkpln5402@gmail.com>
2023-04-28 22:59:48 +0300

    initial commit

---
+++ src/main/java/com/Softito/SoftitoECommerce/SoftitoECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.Softito.SoftitoECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftitoECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftitoECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Softito/SoftitoECommerce/SoftitoECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Softito.SoftitoECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftitoECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
